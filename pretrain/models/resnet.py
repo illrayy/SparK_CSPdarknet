9a8cf9bba124ebc2fdc4128ca9aedb07937b9287
illrayy <93829503+illrayy@users.noreply.github.com>
2023-05-19 21:46:15 +0800

    Update resnet.py

--- pretrain/models/resnet.py
+++ pretrain/models/resnet.py
@@ -3,261 +3,64 @@
 #
 # This source code is licensed under the license found in the
 # LICENSE file in the root directory of this source tree.
+from typing import List
 
 import torch
-import torch.nn as nn
-from typing import List
-from timm.models.registry import register_model
-
-class SiLU(nn.Module):
-    @staticmethod
-    def forward(x):
-        return x * torch.sigmoid(x)
-
-def get_activation(name="silu", inplace=True):
-    if name == "silu":
-        module = SiLU()
-    elif name == "relu":
-        module = nn.ReLU(inplace=inplace)
-    elif name == "lrelu":
-        module = nn.LeakyReLU(0.1, inplace=inplace)
-    else:
-        raise AttributeError("Unsupported act type: {}".format(name))
-    return module
-
-#将focus替换为kernelsize=6，stride=2，padding=2的卷积层
-class Focus(nn.Module):
-    def __init__(self, in_channels, out_channels, ksize=6, stride=2, act="silu"):
-        super().__init__()
-        self.conv = BaseConv(in_channels, out_channels, ksize, stride, act=act)
-
-    def forward(self, x):
-        return self.conv(x)
-
-class BaseConv(nn.Module):
-    def __init__(self, in_channels, out_channels, ksize, stride, groups=1, bias=False, act="silu"):
-        super().__init__()
-        pad         = (ksize - 1) // 2
-        self.conv   = nn.Conv2d(in_channels, out_channels, kernel_size=ksize, stride=stride, padding=pad, groups=groups, bias=bias)
-        self.bn     = nn.BatchNorm2d(out_channels, eps=0.001, momentum=0.03)
-        self.act    = get_activation(act, inplace=True)
-
-    def forward(self, x):
-        return self.act(self.bn(self.conv(x)))
-
-    def fuseforward(self, x):
-        return self.act(self.conv(x))
-    
-class DWConv(nn.Module):
-    def __init__(self, in_channels, out_channels, ksize, stride=1, act="silu"):
-        super().__init__()
-        self.dconv = BaseConv(in_channels, in_channels, ksize=ksize, stride=stride, groups=in_channels, act=act,)
-        self.pconv = BaseConv(in_channels, out_channels, ksize=1, stride=1, groups=1, act=act)
-
-    def forward(self, x):
-        x = self.dconv(x)
-        return self.pconv(x)
-
-class SPPBottleneck(nn.Module):
-    def __init__(self, in_channels, out_channels, kernel_size=5, activation="silu"):
-        super().__init__()
-        hidden_channels = in_channels // 2
-        self.conv1      = BaseConv(in_channels, hidden_channels, 1, stride=1, act=activation)
-        self.m          = nn.MaxPool2d(kernel_size, stride=1, padding=kernel_size // 2)
-        conv2_channels  = hidden_channels * 4
-        self.conv2      = BaseConv(conv2_channels, out_channels, 1, stride=1, act=activation)
-
-    def forward(self, x):
-        x = self.conv1(x)
-        x_m1 = self.m(x)
-        x_m2 = self.m(x_m1)
-        return self.conv2(torch.cat([x, x_m1, x_m2, self.m(x_m2)], 1))
-
-#--------------------------------------------------#
-#   残差结构的构建，小的残差结构
-#--------------------------------------------------#
-class Bottleneck(nn.Module):
-    # Standard bottleneck
-    def __init__(self, in_channels, out_channels, shortcut=True, expansion=0.5, depthwise=False, act="silu",):
-        super().__init__()
-        hidden_channels = int(out_channels * expansion)
-        Conv = DWConv if depthwise else BaseConv
-        #--------------------------------------------------#
-        #   利用1x1卷积进行通道数的缩减。缩减率一般是50%
-        #--------------------------------------------------#
-        self.conv1 = BaseConv(in_channels, hidden_channels, 1, stride=1, act=act)
-        #--------------------------------------------------#
-        #   利用3x3卷积进行通道数的拓张。并且完成特征提取
-        #--------------------------------------------------#
-        self.conv2 = Conv(hidden_channels, out_channels, 3, stride=1, act=act)
-        self.use_add = shortcut and in_channels == out_channels
-
-    def forward(self, x):
-        y = self.conv2(self.conv1(x))
-        if self.use_add:
-            y = y + x
-        return y
-
-class CSPLayer(nn.Module):
-    def __init__(self, in_channels, out_channels, n=1, shortcut=True, expansion=0.5, depthwise=False, act="silu",):
-        # ch_in, ch_out, number, shortcut, groups, expansion
-        super().__init__()
-        hidden_channels = int(out_channels * expansion)  
-        #--------------------------------------------------#
-        #   主干部分的初次卷积
-        #--------------------------------------------------#
-        self.conv1  = BaseConv(in_channels, hidden_channels, 1, stride=1, act=act)
-        #--------------------------------------------------#
-        #   大的残差边部分的初次卷积
-        #--------------------------------------------------#
-        self.conv2  = BaseConv(in_channels, hidden_channels, 1, stride=1, act=act)
-        #-----------------------------------------------#
-        #   对堆叠的结果进行卷积的处理
-        #-----------------------------------------------#
-        self.conv3  = BaseConv(2 * hidden_channels, out_channels, 1, stride=1, act=act)
-
-        #--------------------------------------------------#
-        #   根据循环的次数构建上述Bottleneck残差结构
-        #--------------------------------------------------#
-        module_list = [Bottleneck(hidden_channels, hidden_channels, shortcut, 1.0, depthwise, act=act) for _ in range(n)]
-        self.m      = nn.Sequential(*module_list)
-
-    def forward(self, x):
-        #-------------------------------#
-        #   x_1是主干部分
-        #-------------------------------#
-        x_1 = self.conv1(x)
-        #-------------------------------#
-        #   x_2是大的残差边部分
-        #-------------------------------#
-        x_2 = self.conv2(x)
-
-        #-----------------------------------------------#
-        #   主干部分利用残差结构堆叠继续进行特征提取
-        #-----------------------------------------------#
-        x_1 = self.m(x_1)
-        #-----------------------------------------------#
-        #   主干部分和大的残差边部分进行堆叠
-        #-----------------------------------------------#
-        x = torch.cat((x_1, x_2), dim=1)
-        #-----------------------------------------------#
-        #   对堆叠的结果进行卷积的处理
-        #-----------------------------------------------#
-        return self.conv3(x)
+import torch.nn.functional as F
+from timm.models.resnet import ResNet
 
 
-class CSPDarknet(nn.Module):
-    def __init__(self, dep_mul=1, wid_mul=1, out_features=("dark3", "dark4", "dark5"), depthwise=False, act="silu",):
-        super().__init__()
-        assert out_features, "please provide output features of Darknet"
-        self.out_features = out_features
-        self.wid_mul = wid_mul
-        Conv = DWConv if depthwise else BaseConv
+# hack: inject the `get_downsample_ratio` function into `timm.models.resnet.ResNet`
+def get_downsample_ratio(self: ResNet) -> int:
+    return 32
 
 
-        base_channels   = int(wid_mul * 64)  # 64
-        self.base_channels = base_channels
-        base_depth      = max(round(dep_mul * 3), 1)  # 3
-        
-        #focus
-        #downsample_ratio,ch
-        #2, 64
-        self.stem = Focus(3, base_channels, ksize=3, act=act)
-        
-        #4, 128
-        self.dark2 = nn.Sequential(
-            Conv(base_channels, base_channels * 2, 3, 2, act=act),
-            CSPLayer(base_channels * 2, base_channels * 2, n=base_depth, depthwise=depthwise, act=act),
-        )
+# hack: inject the `get_feature_map_channels` function into `timm.models.resnet.ResNet`
+def get_feature_map_channels(self: ResNet) -> List[int]:
+    # `self.feature_info` is maintained by `timm`
+    return [info['num_chs'] for info in self.feature_info[1:]]
 
-        #8, 256
-        self.dark3 = nn.Sequential(
-            Conv(base_channels * 2, base_channels * 4, 3, 2, act=act),
-            CSPLayer(base_channels * 4, base_channels * 4, n=base_depth * 3, depthwise=depthwise, act=act),
-        )
 
-        #16, 512
-        self.dark4 = nn.Sequential(
-            Conv(base_channels * 4, base_channels * 8, 3, 2, act=act),
-            CSPLayer(base_channels * 8, base_channels * 8, n=base_depth * 3, depthwise=depthwise, act=act),
-        )
-
-        #32, 1024
-        self.dark5 = nn.Sequential(
-            Conv(base_channels * 8, base_channels * 16, 3, 2, act=act),
-            SPPBottleneck(base_channels * 16, base_channels * 16, activation=act),
-            CSPLayer(base_channels * 16, base_channels * 16, n=base_depth, shortcut=False, depthwise=depthwise, act=act),
-        )
-        
+# hack: override the forward function of `timm.models.resnet.ResNet`
+def forward(self, x, hierarchical=False):
+    """ this forward function is a modified version of `timm.models.resnet.ResNet.forward`
+    >>> ResNet.forward
     """
-    This is a template for your custom ConvNet.
-    It is required to implement the following three functions: `get_downsample_ratio`, `get_feature_map_channels`, `forward`.
-    You can refer to the implementations in `pretrain\models\resnet.py` for an example.
-    """
+    x = self.conv1(x)
+    x = self.bn1(x)
+    x = self.act1(x)
+    x = self.maxpool(x)
     
-    def get_downsample_ratio(self) -> int:
-        return 32
-    
-    def get_feature_map_channels(self) -> List[int]:
-        return [4*self.base_channels, 8*self.base_channels, 16*self.base_channels]
-    
-    def forward(self, x, hierarchical=False):
+    if hierarchical:
         ls = []
-        x = self.stem(x)
-        x = self.dark2(x)
-        #-----------------------------------------------#
-        #   dark3 8, 256
-        #-----------------------------------------------#
-        x = self.dark3(x)
-        ls.append(x)
-        #-----------------------------------------------#
-        #   dark4 16, 512
-        #-----------------------------------------------#
-        x = self.dark4(x)
-        ls.append(x)
-        #-----------------------------------------------#
-        #   dark5 32, 1024
-        #-----------------------------------------------#
-        x = self.dark5(x)
-        ls.append(x)
+        x = self.layer1(x); ls.append(x)
+        x = self.layer2(x); ls.append(x)
+        x = self.layer3(x); ls.append(x)
+        x = self.layer4(x); ls.append(x)
         return ls
+    else:
+        x = self.global_pool(x)
+        if self.drop_rate:
+            x = F.dropout(x, p=float(self.drop_rate), training=self.training)
+        x = self.fc(x)
+        return x
 
 
-@register_model
-def CSPDarknet_nano(pretrained=False, **kwargs):
-    model = CSPDarknet(0.33, 0.25, depthwise=True)
-    return model
+ResNet.get_downsample_ratio = get_downsample_ratio
+ResNet.get_feature_map_channels = get_feature_map_channels
+ResNet.forward = forward
 
-def CSPDarknet_tiny(pretrained=False, **kwargs):
-    model = CSPDarknet(0.33, 0.375)
-    return model
-
-def CSPDarknet_s(pretrained=False, **kwargs):
-    model = CSPDarknet(0.33, 0.5)
-    return model
-
-def CSPDarknet_m(pretrained=False, **kwargs):
-    model = CSPDarknet(0.67, 0.75)
-    return model
-
-def CSPDarknet_l(pretrained=False, **kwargs):
-    model = CSPDarknet(1, 1)
-    return model
-
-def CSPDarknet_x(pretrained=False, **kwargs):
-    model = CSPDarknet(1.33, 1.25)
-    return model
 
 @torch.no_grad()
 def convnet_test():
     from timm.models import create_model
-    cnn = create_model('CSPDarknet_nano')
+    cnn = create_model('resnet50')
     print('get_downsample_ratio:', cnn.get_downsample_ratio())
     print('get_feature_map_channels:', cnn.get_feature_map_channels())
     
     downsample_ratio = cnn.get_downsample_ratio()
     feature_map_channels = cnn.get_feature_map_channels()
-    
+
     # check the forward function
     B, C, H, W = 4, 3, 224, 224
     inp = torch.rand(B, C, H, W)
@@ -265,12 +68,12 @@ def convnet_test():
     assert isinstance(feats, list)
     assert len(feats) == len(feature_map_channels)
     print([tuple(t.shape) for t in feats])
-    
+
     # check the downsample ratio
     feats = cnn(inp, hierarchical=True)
     assert feats[-1].shape[-2] == H // downsample_ratio
     assert feats[-1].shape[-1] == W // downsample_ratio
-    
+
     # check the channel number
     for feat, ch in zip(feats, feature_map_channels):
         assert feat.ndim == 4
