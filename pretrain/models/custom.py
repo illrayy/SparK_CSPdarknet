221f05e518f710024837872bd5b11f466b50e6bb
illrayy <93829503+illrayy@users.noreply.github.com>
2023-05-19 21:36:00 +0800

    Update custom.py

--- pretrain/models/custom.py
+++ pretrain/models/custom.py
@@ -10,7 +10,48 @@ from typing import List
 from timm.models.registry import register_model
 
 
-class YourConvNet(nn.Module):
+class CSPDarknet(nn.Module):
+    def __init__(self, dep_mul=1, wid_mul=1, out_features=("dark3", "dark4", "dark5"), depthwise=False, act="silu",):
+        super().__init__()
+        assert out_features, "please provide output features of Darknet"
+        self.out_features = out_features
+        self.wid_mul = wid_mul
+        Conv = DWConv if depthwise else BaseConv
+
+
+        base_channels   = int(wid_mul * 64)  # 64
+        base_depth      = max(round(dep_mul * 3), 1)  # 3
+        
+        #focus
+        #downsample_ratio,ch
+        #2, 64
+        self.stem = Focus(3, base_channels, ksize=3, act=act)
+        
+        #4, 128
+        self.dark2 = nn.Sequential(
+            Conv(base_channels, base_channels * 2, 3, 2, act=act),
+            CSPLayer(base_channels * 2, base_channels * 2, n=base_depth, depthwise=depthwise, act=act),
+        )
+
+        #8, 256
+        self.dark3 = nn.Sequential(
+            Conv(base_channels * 2, base_channels * 4, 3, 2, act=act),
+            CSPLayer(base_channels * 4, base_channels * 4, n=base_depth * 3, depthwise=depthwise, act=act),
+        )
+
+        #16, 512
+        self.dark4 = nn.Sequential(
+            Conv(base_channels * 4, base_channels * 8, 3, 2, act=act),
+            CSPLayer(base_channels * 8, base_channels * 8, n=base_depth * 3, depthwise=depthwise, act=act),
+        )
+
+        #32, 1024
+        self.dark5 = nn.Sequential(
+            Conv(base_channels * 8, base_channels * 16, 3, 2, act=act),
+            SPPBottleneck(base_channels * 16, base_channels * 16, activation=act),
+            CSPLayer(base_channels * 16, base_channels * 16, n=base_depth, shortcut=False, depthwise=depthwise, act=act),
+        )
+        
     """
     This is a template for your custom ConvNet.
     It is required to implement the following three functions: `get_downsample_ratio`, `get_feature_map_channels`, `forward`.
@@ -18,48 +59,62 @@ class YourConvNet(nn.Module):
     """
     
     def get_downsample_ratio(self) -> int:
-        """
-        This func would ONLY be used in `SparseEncoder's __init__` (see `pretrain/encoder.py`).
-        
-        :return: the TOTAL downsample ratio of the ConvNet.
-        E.g., for a ResNet-50, this should return 32.
-        """
-        raise NotImplementedError
+        return 32
     
     def get_feature_map_channels(self) -> List[int]:
-        """
-        This func would ONLY be used in `SparseEncoder's __init__` (see `pretrain/encoder.py`).
-        
-        :return: a list of the number of channels of each feature map.
-        E.g., for a ResNet-50, this should return [256, 512, 1024, 2048].
-        """
-        raise NotImplementedError
+        return [256, 512, 1024]*self.wid_mul
     
-    def forward(self, inp_bchw: torch.Tensor, hierarchical=False):
-        """
-        The forward with `hierarchical=True` would ONLY be used in `SparseEncoder.forward` (see `pretrain/encoder.py`).
-        
-        :param inp_bchw: input image tensor, shape: (batch_size, channels, height, width).
-        :param hierarchical: return the logits (not hierarchical), or the feature maps (hierarchical).
-        :return:
-            - hierarchical == False: return the logits of the classification task, shape: (batch_size, num_classes).
-            - hierarchical == True: return a list of all feature maps, which should have the same length as the return value of `get_feature_map_channels`.
-              E.g., for a ResNet-50, it should return a list [1st_feat_map, 2nd_feat_map, 3rd_feat_map, 4th_feat_map].
-                    for an input size of 224, the shapes are [(B, 256, 56, 56), (B, 512, 28, 28), (B, 1024, 14, 14), (B, 2048, 7, 7)]
-        """
-        raise NotImplementedError
+    def forward(self, x, hierarchical=False):
+        ls = []
+        x = self.stem(x)
+        x = self.dark2(x)
+        #-----------------------------------------------#
+        #   dark3 8, 256
+        #-----------------------------------------------#
+        x = self.dark3(x)
+        ls.append(x)
+        #-----------------------------------------------#
+        #   dark4 16, 512
+        #-----------------------------------------------#
+        x = self.dark4(x)
+        ls.append(x)
+        #-----------------------------------------------#
+        #   dark5 32, 1024
+        #-----------------------------------------------#
+        x = self.dark5(x)
+        ls.append(x)
+        return ls
 
 
 @register_model
-def your_convnet_small(pretrained=False, **kwargs):
-    raise NotImplementedError
-    return YourConvNet(**kwargs)
+def CSPDarknet_nano(pretrained=False, **kwargs):
+    model = CSPDarknet(0.33, 0.25, depthwise=True)
+    return model
 
+def CSPDarknet_tiny(pretrained=False, **kwargs):
+    model = CSPDarknet(0.33, 0.375)
+    return model
+
+def CSPDarknet_s(pretrained=False, **kwargs):
+    model = CSPDarknet(0.33, 0.5)
+    return model
+
+def CSPDarknet_m(pretrained=False, **kwargs):
+    model = CSPDarknet(0.67, 0.75)
+    return model
+
+def CSPDarknet_l(pretrained=False, **kwargs):
+    model = CSPDarknet(1, 1)
+    return model
+
+def CSPDarknet_x(pretrained=False, **kwargs):
+    model = CSPDarknet(1.33, 1.25)
+    return model
 
 @torch.no_grad()
 def convnet_test():
     from timm.models import create_model
-    cnn = create_model('your_convnet_small')
+    cnn = create_model('CSPDarknet_nano')
     print('get_downsample_ratio:', cnn.get_downsample_ratio())
     print('get_feature_map_channels:', cnn.get_feature_map_channels())
     
