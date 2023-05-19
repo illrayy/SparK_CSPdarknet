19cbdc8da0795cbcb2b7e20b79a216918e1fec53
illrayy <93829503+illrayy@users.noreply.github.com>
2023-05-19 21:45:28 +0800

    Update custom.py

--- pretrain/models/custom.py
+++ pretrain/models/custom.py
@@ -9,6 +9,142 @@ import torch.nn as nn
 from typing import List
 from timm.models.registry import register_model
 
+class SiLU(nn.Module):
+    @staticmethod
+    def forward(x):
+        return x * torch.sigmoid(x)
+
+def get_activation(name="silu", inplace=True):
+    if name == "silu":
+        module = SiLU()
+    elif name == "relu":
+        module = nn.ReLU(inplace=inplace)
+    elif name == "lrelu":
+        module = nn.LeakyReLU(0.1, inplace=inplace)
+    else:
+        raise AttributeError("Unsupported act type: {}".format(name))
+    return module
+
+#将focus替换为kernelsize=6，stride=2，padding=2的卷积层
+class Focus(nn.Module):
+    def __init__(self, in_channels, out_channels, ksize=6, stride=2, act="silu"):
+        super().__init__()
+        self.conv = BaseConv(in_channels, out_channels, ksize, stride, act=act)
+
+    def forward(self, x):
+        return self.conv(x)
+
+class BaseConv(nn.Module):
+    def __init__(self, in_channels, out_channels, ksize, stride, groups=1, bias=False, act="silu"):
+        super().__init__()
+        pad         = (ksize - 1) // 2
+        self.conv   = nn.Conv2d(in_channels, out_channels, kernel_size=ksize, stride=stride, padding=pad, groups=groups, bias=bias)
+        self.bn     = nn.BatchNorm2d(out_channels, eps=0.001, momentum=0.03)
+        self.act    = get_activation(act, inplace=True)
+
+    def forward(self, x):
+        return self.act(self.bn(self.conv(x)))
+
+    def fuseforward(self, x):
+        return self.act(self.conv(x))
+    
+class DWConv(nn.Module):
+    def __init__(self, in_channels, out_channels, ksize, stride=1, act="silu"):
+        super().__init__()
+        self.dconv = BaseConv(in_channels, in_channels, ksize=ksize, stride=stride, groups=in_channels, act=act,)
+        self.pconv = BaseConv(in_channels, out_channels, ksize=1, stride=1, groups=1, act=act)
+
+    def forward(self, x):
+        x = self.dconv(x)
+        return self.pconv(x)
+
+class SPPBottleneck(nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size=5, activation="silu"):
+        super().__init__()
+        hidden_channels = in_channels // 2
+        self.conv1      = BaseConv(in_channels, hidden_channels, 1, stride=1, act=activation)
+        self.m          = nn.MaxPool2d(kernel_size, stride=1, padding=kernel_size // 2)
+        conv2_channels  = hidden_channels * 4
+        self.conv2      = BaseConv(conv2_channels, out_channels, 1, stride=1, act=activation)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x_m1 = self.m(x)
+        x_m2 = self.m(x_m1)
+        return self.conv2(torch.cat([x, x_m1, x_m2, self.m(x_m2)], 1))
+
+#--------------------------------------------------#
+#   残差结构的构建，小的残差结构
+#--------------------------------------------------#
+class Bottleneck(nn.Module):
+    # Standard bottleneck
+    def __init__(self, in_channels, out_channels, shortcut=True, expansion=0.5, depthwise=False, act="silu",):
+        super().__init__()
+        hidden_channels = int(out_channels * expansion)
+        Conv = DWConv if depthwise else BaseConv
+        #--------------------------------------------------#
+        #   利用1x1卷积进行通道数的缩减。缩减率一般是50%
+        #--------------------------------------------------#
+        self.conv1 = BaseConv(in_channels, hidden_channels, 1, stride=1, act=act)
+        #--------------------------------------------------#
+        #   利用3x3卷积进行通道数的拓张。并且完成特征提取
+        #--------------------------------------------------#
+        self.conv2 = Conv(hidden_channels, out_channels, 3, stride=1, act=act)
+        self.use_add = shortcut and in_channels == out_channels
+
+    def forward(self, x):
+        y = self.conv2(self.conv1(x))
+        if self.use_add:
+            y = y + x
+        return y
+
+class CSPLayer(nn.Module):
+    def __init__(self, in_channels, out_channels, n=1, shortcut=True, expansion=0.5, depthwise=False, act="silu",):
+        # ch_in, ch_out, number, shortcut, groups, expansion
+        super().__init__()
+        hidden_channels = int(out_channels * expansion)  
+        #--------------------------------------------------#
+        #   主干部分的初次卷积
+        #--------------------------------------------------#
+        self.conv1  = BaseConv(in_channels, hidden_channels, 1, stride=1, act=act)
+        #--------------------------------------------------#
+        #   大的残差边部分的初次卷积
+        #--------------------------------------------------#
+        self.conv2  = BaseConv(in_channels, hidden_channels, 1, stride=1, act=act)
+        #-----------------------------------------------#
+        #   对堆叠的结果进行卷积的处理
+        #-----------------------------------------------#
+        self.conv3  = BaseConv(2 * hidden_channels, out_channels, 1, stride=1, act=act)
+
+        #--------------------------------------------------#
+        #   根据循环的次数构建上述Bottleneck残差结构
+        #--------------------------------------------------#
+        module_list = [Bottleneck(hidden_channels, hidden_channels, shortcut, 1.0, depthwise, act=act) for _ in range(n)]
+        self.m      = nn.Sequential(*module_list)
+
+    def forward(self, x):
+        #-------------------------------#
+        #   x_1是主干部分
+        #-------------------------------#
+        x_1 = self.conv1(x)
+        #-------------------------------#
+        #   x_2是大的残差边部分
+        #-------------------------------#
+        x_2 = self.conv2(x)
+
+        #-----------------------------------------------#
+        #   主干部分利用残差结构堆叠继续进行特征提取
+        #-----------------------------------------------#
+        x_1 = self.m(x_1)
+        #-----------------------------------------------#
+        #   主干部分和大的残差边部分进行堆叠
+        #-----------------------------------------------#
+        x = torch.cat((x_1, x_2), dim=1)
+        #-----------------------------------------------#
+        #   对堆叠的结果进行卷积的处理
+        #-----------------------------------------------#
+        return self.conv3(x)
+
 
 class CSPDarknet(nn.Module):
     def __init__(self, dep_mul=1, wid_mul=1, out_features=("dark3", "dark4", "dark5"), depthwise=False, act="silu",):
@@ -20,6 +156,7 @@ class CSPDarknet(nn.Module):
 
 
         base_channels   = int(wid_mul * 64)  # 64
+        self.base_channels = base_channels
         base_depth      = max(round(dep_mul * 3), 1)  # 3
         
         #focus
@@ -62,7 +199,7 @@ class CSPDarknet(nn.Module):
         return 32
     
     def get_feature_map_channels(self) -> List[int]:
-        return [256, 512, 1024]*self.wid_mul
+        return [4*self.base_channels, 8*self.base_channels, 16*self.base_channels]
     
     def forward(self, x, hierarchical=False):
         ls = []
